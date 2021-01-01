24dfdd7f1a529487caebfdc6f321e0197db68877
Adrien Guinet <adrien@guinet.me>
2021-01-01 22:14:11 +0100

    Introduce Dockerfile and a Github action workflow
    This test the app against Speculos for Nano S.

---
+++ ci/docker_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ex
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 source_dir"
+  exit 1
+fi
+
+SRCDIR="$1"
+
+# Build libkpl
+KPL_BUILDDIR="$SRCDIR/libkpl/build_ci"
+rm -rf "$KPL_BUILDDIR"
+mkdir "$KPL_BUILDDIR"
+pushd "$KPL_BUILDDIR"
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make -j$(nproc)
+popd
+
+# Build the app
+pushd "$SRCDIR/app"
+export BOLOS_SDK=$NANOS_SDK
+make clean && make -j$(nproc)
+popd
+
+# Run the tests
+"$SRCDIR/tests/run.sh" /opt/speculos/speculos.py "$KPL_BUILDDIR" nanos
