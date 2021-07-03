942c4a36368f4dde9232383285720ce1d8738ca0
Adrien Guinet <adrien@guinet.me>
2021-07-03 12:07:42 +0200

    libkpl: support ERASE_ALL_SLOTS instruction + tool

--- libkpl/src/instrs.h
+++ libkpl/src/instrs.h
@@ -8,7 +8,8 @@ enum Instrs : uint8_t {
   STORE_KEY = 1,
   GET_KEY = 2,
   GET_KEY_FROM_NAME = 3,
-  GET_VALID_KEY_SLOTS = 4
+  GET_VALID_KEY_SLOTS = 4,
+  ERASE_ALL_SLOTS = 5
 };
 
 } // namespace kpl
